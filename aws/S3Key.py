ee9a1070c4e6ec0134bc9ce029d6052ee53cf16d
Kyle Al-Shafei <kalshafei@gmail.com>
2019-12-30 22:27:05 -0500

    add generated unsigned url method

--- aws/S3Key.py
+++ aws/S3Key.py
@@ -3,7 +3,10 @@ import os
 import os.path
 import boto3
 import botocore
+import logging
 from typing import BinaryIO, Callable, List, Union
+from botocore.exceptions import ClientError
+
 
 class S3Key:
     """An Amazon S3 Key"""
@@ -132,3 +135,50 @@ class S3Key:
             Params={"Bucket": self.bucket.name, "Key": self.name},
             ExpiresIn=duration,
         )
+
+    def create_presigned_url(self, expiration=3600):
+        """Generate a presigned URL to share an S3 object
+        :param expiration: Time in seconds for the presigned URL to remain valid
+        :return: Presigned URL as string. If error, returns None.
+        """
+        try: 
+            response = self.bucket._boto_s3.meta.client.generate_presigned_url(
+                "get_object",
+                Params= {"Bucket": self.bucket.name, "Key": self.name},
+                ExpiresIn=expiration,
+            )
+        except ClientError as e:
+            logging.error(e)
+            print(e)
+            return None
+        
+        logging.info(f'Presigned URL to share an S3 object: {url}')
+        return response
+
+    def create_presigned_url_expanded(self, client_method_name, method_parameters=None,
+                                  expiration=3600, http_method=None):
+    """Generate a presigned URL to invoke an S3.Client method
+
+    Not all the client methods provided in the AWS Python SDK are supported.
+
+    :param client_method_name: Name of the S3.Client method, e.g., 'list_buckets'
+    :param method_parameters: Dictionary of parameters to send to the method
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :param http_method: HTTP method to use (GET, etc.)
+    :return: Presigned URL as string. If error, returns None.
+    """
+    # Generate a presigned URL for the S3 client method
+    s3_client = boto3.client('s3')
+    try:
+        response = self.bucket._boto_s3.meta.client.generate_presigned_url(
+                ClientMethod=client_method_name,                                  
+                Params=method_parameters,
+                ExpiresIn=expiration,
+                HttpMethod=http_method
+                )
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    logging.info(f'Presigned URL to share an S3 object: {url}')
+    return response
